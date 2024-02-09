13d9835756bbb459c9455bea7b1ee037adb92c74
sandipto729 <sandipto729@gmail.com>
2024-02-09 18:25:10 +0530

    Change Game for n*n

--- game.cpp
+++ game.cpp
@@ -1,78 +1,133 @@
 #include<iostream>
 #include<vector>
-#include<string.h>
+#include<string>
 using namespace std;
 
-char arr[3][3] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-int row, col;
-char token = 'x';
+string** arr;
+int n, row, col;
+string token = "x";
 string name1 = "", name2 = "";
 bool tie;
 
-// This function makes playing space for players
-void function1() {
-    // Making gaming place
-    cout << "   |   |   \n";
-    cout << " " << arr[0][0] << " | " << arr[0][1] << " | " << arr[0][2] << "\n";
-    cout << "___|___|___\n";
-    cout << "   |   |   \n";
-    cout << " " << arr[1][0] << " | " << arr[1][1] << " | " << arr[1][2] << "\n";
-    cout << "___|___|___\n";
-    cout << "   |   |   \n";
-    cout << " " << arr[2][0] << " | " << arr[2][1] << " | " << arr[2][2] << "\n";
-    cout << "   |   |   \n";
-}
-
-// This function takes input from the user
-void function2() {
-    // Taking position value from the user
-    int digit;
-    if (token == 'x') {
-        cout << name1 << " please Enter : ";
-        cin >> digit;
-    }
-    if (token == '0') {
-        cout << name2 << " please Enter : ";
-        cin >> digit;
-    }
-    if (digit >= 1 && digit <= 9) {
-        row = (digit - 1) / 3;
-        col = (digit - 1) % 3;
-    }
-    else {
-        cout << "Invalid !!! ";
-    }
-
-    // Put the value
-    if (token == 'x' && arr[row][col] != 'x' && arr[row][col] != '0') {
-        arr[row][col] = 'x';
-        token = '0';
-    }
-    else if (token == '0' && arr[row][col] != 'x' && arr[row][col] != '0') {
-        arr[row][col] = '0';
-        token = 'x';
-    }
-    else {
-        cout << "Sorry no space is empty !!! ";
-        function2();
-    }
-    function1();
-}
-
-bool function3() {
-    for (int i = 0; i < 3; i++) {
-        if ((arr[i][0] == arr[i][1] && arr[i][1] == arr[i][2]) || (arr[0][i] == arr[1][i] && arr[0][i] == arr[2][i])) {
-            return true;
+// Function to initialize the game board
+void initializeBoard() {
+    int l=1;
+    arr = new string*[n];
+    for (int i = 0; i < n; ++i) {
+        arr[i] = new string[n];
+        for (int j = 0; j < n; ++j) {
+            arr[i][j] = to_string(l); // Fill the board with numbers 1 to n*n
+            l++;
         }
     }
-    if ((arr[0][0] == arr[1][1] && arr[0][0] == arr[2][2]) || (arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0])) {
-        return true;
+}
+
+// Function to display the game board
+void displayBoard() {
+
+    for(int j=1;j<=n+1;j++){
+            cout<<"___";
+        }
+        cout<<endl;
+    for(int i=0;i<(n);i++){
+        
+        cout << "   |";
+        for(int j=1;j<=n-1;j++){
+            cout<<"   |";
+        }
+        cout<<"   \n";
+
+        cout<<" ";
+        for(int j=0;j<n;j++){
+            if(arr[i][j].size()==1){
+            cout<<arr[i][j]<<" | ";}
+            else{
+                cout<<arr[i][j]<<"| ";
+            }
+        }
+        cout<<"\n";
+        cout<<"___|";
+        for(int j=1;j<=n-1;j++){
+            cout<<"___|";
+        }
+        //cout<<"___\n";
+        cout<<"\n";
     }
+}
+
+// Function to take input from the user
+void getInput() {
+    int digit;
+    cout << (token == "x" ? name1 : name2) << " please enter: ";
+    cin >> digit;
+
+    // Convert input to row and column
+    row = (digit - 1) / n;
+    col = (digit - 1) % n;
+
+    // Check for valid input
+    if (digit < 1 || digit > n * n || arr[row][col] == "x" || arr[row][col] == "0") {
+        cout << "Invalid input! Please try again." << endl;
+        getInput();
+    }
+}
+
+// Function to update the game board
+void updateBoard() {
+    arr[row][col] = token;
+    token = (token == "x") ? "0" : "x";
+}
+
+// Function to check for a win or tie
+bool checkResult() {
+    //check rows and coloum
+   
+    
+    for(int i=0;i<n;i++){
+        string pivot1=arr[i][0];
+        bool rowCheck=true;
+        for(int j=1;j<n;j++){
+           // cout<<arr[i][j]<<" ";
+            if(pivot1!=arr[i][j])rowCheck=false;
+
+        }
+        if(rowCheck)return true;
+    }
+   
+    
+    for(int i=0;i<n;i++){
+        string pivot2=arr[0][i];
+        bool colCheck=true;
+        for(int j=1;j<n;j++){
+            if(pivot2!=arr[j][i])colCheck=false;
+        }
+        if(colCheck)return true;
+    }
+    
+
+    // check diagonal
+
+    bool diagonalCheck=true;
+    string pivot3=arr[0][0];
+    for(int i=1;i<n;i++){
+        if(pivot3!=arr[i][i])diagonalCheck=false;
+    }
+    if(diagonalCheck)return true;
+
+
+    //check antidiagonal
+
+    bool antidiagonalcheck=true;
+    string pivot4=arr[n-1][0];
+    for(int i=1;i<n;i++){
+        if(pivot4!=arr[n-1-i][i])antidiagonalcheck=false;
+    }
+    if(antidiagonalcheck)return true;
 
     // Check for a tie
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (arr[i][j] != 'x' && arr[i][j] != '0') {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (arr[i][j] != "x" && arr[i][j] != "0") {
                 return false;
             }
         }
@@ -83,26 +138,40 @@ bool function3() {
     return true;
 }
 
-
+// Main function
 int main() {
-    cout << "Enter name of first player : ";
-    getline(cin, name1);
-    cout << "Enter name of the second player : ";
-    getline(cin, name2);
-    function1();
-    while (!function3()) {
-        function2();
-        function3();
+    cout << "Enter the size of the board (n x n): ";
+    cin >> n;
+
+    initializeBoard();
+
+    cout << "Enter name of the first player: ";
+    cin >> name1;
+    cout << "Enter name of the second player: ";
+    cin >> name2;
+
+    displayBoard();
+
+    while (!checkResult()) {
+        getInput();
+        updateBoard();
+        displayBoard();
+        checkResult();
     }
-    if (token == 'x' && tie == false) {
-        cout << name2 << " Wins the game" << endl;
-    }
-    else if (token == '0' && tie == false) {
-        cout << name1 << " Wins the game" << endl;
-    }
-    else {
+
+    if (token == "x" && !tie) {
+        cout << name2 << " wins the game" << endl;
+    } else if (token == "0" && !tie) {
+        cout << name1 << " wins the game" << endl;
+    } else {
         cout << "It's a draw" << endl;
     }
+
+    // Deallocate memory
+    for (int i = 0; i < n; ++i) {
+        delete[] arr[i];
+    }
+    delete[] arr;
 
     return 0;
 }
